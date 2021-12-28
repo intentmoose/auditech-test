dacd613287ef3e0af9adfb3d9922d71af32997cc
intentmoose <cariel226@gmail.com>
2021-12-28 23:19:41 +0200

    added user settings with logout

---
+++ src/components/UserSettings.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="text-center">
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn icon dark v-bind="attrs" v-on="on">
+          <v-avatar>
+            <v-icon dark> mdi-account-circle </v-icon>
+          </v-avatar>
+        </v-btn>
+      </template>
+      <v-btn @click="logOut">log out</v-btn>
+    </v-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserSettings",
+  methods: {
+    logOut() {
+      localStorage.removeItem("token");
+      location.reload();
+    },
+  },
+};
+</script>
