8eac4d4b1db9dc55707b6cbdf173cd0b401f10a2
intentmoose <cariel226@gmail.com>
2022-01-09 17:13:50 +0200

    removed vuex since its not in use and added vue store. removed local node url

--- src/main.js
+++ src/main.js
@@ -1,21 +1,16 @@
 import Vue from 'vue';
 import App from './App.vue';
-import store from './store';
 import vuetify from './plugins/vuetify';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 
-// Decide which server to use by detecting if we are in production or not
-process.env.NODE_ENV === 'production'
-  ? (axios.defaults.baseURL = 'https://auditeck-backend.herokuapp.com/')
-  : (axios.defaults.baseURL = 'http://localhost:3000/');
+axios.defaults.baseURL = 'https://auditeck-backend.herokuapp.com/'
 
 Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  store,
   vuetify,
   render: (h) => h(App),
 }).$mount('#app');
